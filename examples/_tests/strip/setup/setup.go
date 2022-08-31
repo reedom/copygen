0c6c0189a0912d258d0724b570b33654b6b61dfd
HANAI tohru <tohru@reedom.com>
2022-08-31 17:29:07 +0900

    test: add stip test

---
+++ examples/_tests/strip/setup/setup.go
@@ -0,0 +1,18 @@
+// Generator should strip either "//go:build ignore" or "go:build exclude" line.
+//go:build ignore
+
+// Package copygen contains the setup information for copygen generated code.
+package copygen
+
+import (
+	service "github.com/switchupcb/copygen/examples/main/domain"
+	data "github.com/switchupcb/copygen/examples/main/models"
+)
+
+// Generator should strip "//go:generate line.
+//go:generate go run github.com/switchupcb/copygen -yml setup.yml
+
+// Copygen defines the functions that will be generated.
+type Copygen interface {
+	ModelsToDomain(*data.Account) *service.Account
+}
